78703797266e2e51cd0072ab78e11430af5e078f
radu <radustanciu841@yahoo.ro>
2021-07-22 11:43:50 +0300

    Add method filter

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,60 @@
-import React, { Component } from 'react';
-import Header from '../components/Header';
-import Navbar from '../components/Navbar';
-import foods from '../utils/foods.json';
-import FoodList from '../components/FoodList';
+import React, { Component } from "react";
+import Header from "../components/Header";
+import foods from "../utils/foods.json";
+import FoodList from "../components/FoodList";
+import '../components/Navbar.css';
 
 export class Home extends Component {
-    constructor(){
-        super();
-        this.state={
-            foods:[]
-        };
-    }
-    componentDidMount(){
-        this.setState({foods:foods})
-    }
-    render() {
-        return (
-            <div>
-                <Header />
-                <Navbar />
-                <FoodList foods={this.state.foods}/>
-            </div>
-        );
-    }
+  constructor() {
+    super();
+    this.state = {
+      foods: [],
+    };
+  }
+//   request catre back-edn
+  componentDidMount() {
+    this.setState({ foods: foods });
+  }
+
+  // filtrari
+  handleAllFoods(event) {
+    this.setState({ foods: foods });
+  }
+
+  handleFilterByBreakfast(event) {
+    const filterFoodsByBreakfast = this.state.foods.filter(
+      (food) => food.name === "American Pancakes" || food.name === "Breakfast"
+    );
+    this.setState({ foods: filterFoodsByBreakfast });
+  }
+  
+  handleFilterByLunch(event) {
+    const filterFoodsByLunch = this.state.foods.filter(
+      (food) => food.name === "American Burger" || food.name === "Max Burger" || food.name==="Bacon Burger"
+    );
+    this.setState({ foods: filterFoodsByLunch });
+  }
+  handleFilterByDinner(event) {
+    const filterFoodsByDinner = this.state.foods.filter(
+      (food) => food.name === "American dinner" || food.name === "Oreo dream cake" 
+    );
+    this.setState({ foods: filterFoodsByDinner });
+  }
+  
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="container-navbar">
+          <button type="button" onClick={(event)=>this.handleAllFoods(event)}>All</button>
+          <button type="button" onClick={(event)=>this.handleFilterByBreakfast(event)}>Breakfast</button>
+          <button type="button" onClick={(event)=>this.handleFilterByLunch(event)}>Lunch</button>
+          <button type="button" onClick={(event)=>this.handleFilterByDinner(event)}>Dinner</button>
+        </div>
+        <FoodList foods={this.state.foods} />
+      </div>
+    );
+  }
 }
 
 export default Home;
